b1bd461d11f5be953ea0b2b9dc8bf94686f731af
sleek-ennju <ihenachochukwudi@gmail.com>
2024-04-22 12:07:13 +0100

    adjusted gap in the hero section

--- src/components/landingPage/header/HeroSection.jsx
+++ src/components/landingPage/header/HeroSection.jsx
@@ -4,7 +4,7 @@ import prize from "../../../assets/images/superCar3.png";
 
 function HeroSection() {
   return (
-    <section className="mx-3 sm:mx-4 md:mx-6 lg:mx-20 grid grid-cols-1 md:grid-cols-2 md:items-center md:gap-8">
+    <section className="mx-3 sm:mx-4 md:mx-6 lg:mx-20 grid grid-cols-1 md:grid-cols-2 md:items-center gap-4 md:gap-8">
       <div className="flex flex-col gap-1">
         <h3 className="text-base sm:text-lg lg:text-xl text-yellowShine uppercase font-dmsans font-semibold">Contest for your chance to</h3>
         <h1 className="text-4xl sm:text-6xl lg:text-7xl text-white uppercase font-lato font-bold tracking-wide">Win Big </h1>
